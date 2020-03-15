6bfc1a26cef808ec7dd4d253729b0cc1a440d627
Robert Hsu <roberthsu2003@gmail.com>
2020-03-15 16:30:58 +0800

    modify

---
+++ 上課內容/weatherman.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def inWeather1():
+    print("weather1")
+    
+def inWeather2():
+    print("weather2")
+
+def inWeather3():
+    print("weather3")
