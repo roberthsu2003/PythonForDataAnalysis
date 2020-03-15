4b12a08f718e88a764b873ffa17b33b5beff0fca
Robert Hsu <roberthsu2003@gmail.com>
2020-03-15 17:00:13 +0800

    modify

---
+++ 上課內容/myPackage/a1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 16:47:43 2020
+
+@author: roberthsu2003
+"""
+
+def a1fun():
+    print("a1Fun")
---
+++ 上課內容/myPackage/a2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 16:48:02 2020
+
+@author: roberthsu2003
+"""
+
+def a2fun():
+    print("a2Fun")
