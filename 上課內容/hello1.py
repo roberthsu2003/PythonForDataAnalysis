43bccd4df12e3bef68d2e8972ee4aebda7de0a5b
Robert Hsu <roberthsu2003@gmail.com>
2020-03-15 13:37:47 +0800

    modify

---
+++ 上課內容/hello1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 12:16:39 2020
+
+@author: roberthsu2003
+"""
+
---
+++ 上課內容/hello2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 12:19:31 2020
+
+@author: roberthsu2003
+"""
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+plt.show()
