e16caa05d2f5ceb5efd6dde559caea72840bd611
roberthsu2003 <roberthsu2003@gmail.com>
2020-08-09 09:03:04 +0800

    modify

--- 上課內容/hello.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 15 12:11:14 2020
-
-@author: roberthsu2003
-"""
-print("Hello!spyder")
--- 上課內容/myPackage/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 15 16:51:09 2020
-
-@author: roberthsu2003
-"""
-
