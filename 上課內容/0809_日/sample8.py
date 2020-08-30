b9cb83cd150917cca16e394f658ce1c677440b5a
roberthsu2003 <roberthsu2003@gmail.com>
2020-08-30 11:48:36 +0800

    modify

--- 上課內容/0809_日/sample8.py
+++ 上課內容/0809_日/sample8.py
@@ -1,6 +1,8 @@
 import re
-
+'''
+bogusemail123@sillymail.com, ,,,roberthsu2003@gmail.com,,,robert@gmail.com
+'''
 regular = r'\w+[.|\w]\w+@\w+[.]\w+[.|\w+]\w+'
 emails = input('請輸入文字,搜尋所有的email:')
-findallObject=re.findall(regular, emails)
-print(findallObject)
+findallList=re.findall(regular, emails)
+print(findallList)
